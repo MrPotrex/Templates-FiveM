9d7a0d5cbb23cddd333240a9dd4850a023af972c
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-10 06:40:09 +0200

    Add files via upload

---
+++ Scripts/fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'adamant'
+game 'gta5'
+
+autor 'Mancotrex'
+name 'Nombre del script'
+description 'Descripción del script'
+version '0.0.0'
+
+shared_scripts {
+	'locales/*.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+-- Si tiene archivos png, css, html, etc... Usarlo, si no, no hace falta.
+files {
+	'html/*.html',
+	'html/fonts/*.ttf',
+	'html/img/*.png',
+	'html/css/*.css',
+	'html/scripts/*.js'
+}
+-- Si tiene alguna interfaz usalo, si no, no.
+ui_page 'html/ui.html'
+
+-- Si tiene alguna dependencia usalo, si no, no.
+--dependency 'nombre'
