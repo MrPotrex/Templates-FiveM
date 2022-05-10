b0ea6c817f18246b0e0554a734edcbe6551c4d01
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-10 06:43:01 +0200

    Add files via upload

---
+++ Mapeados/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean' -- Si te falla esta versión, usa: 'adamant'
+game 'gta5'
+
+author 'Mancotrex'
+name 'Nombre del mapeado'
+description 'Manifest para mapeados'
+version '0.0'
+
+files {
+    'data/*.meta',
+}
+
+-- AÑADIR SI TIENEN ESTOS ARCHIVOS:
+--data_file 'CONTENT_UNLOCKING_META_FILE' 'data/ganjarimscontentunlocks.meta' 
