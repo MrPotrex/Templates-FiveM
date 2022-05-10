9f79ee4767f820c922ffca2a820f83c958fc2a0d
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-10 06:33:24 +0200

    Add files via upload

---
+++ Vehiculos/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mancotrex'
+name 'Manifest para vehiculos'
+description 'Este fxmanifest es para vehiculos'
+version '0.0'
+
+files {
+  'data/*.meta',
+}
+
+-- VEHICULOS DATA FILES
+ data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+ data_file 'HANDLING_FILE' 'data/handling.meta'
+ data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+ data_file 'CARCOLS_FILE' 'data/carcols.meta'
+
+-- AÑADIR SI TE TRAEN ESTOS ARCHIVOS
+-- data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+-- data_file 'VEHICLEEXTRAS_FILE' 'data/vehicleextras.meta'
+-- data_file 'VEHICLE_SHOP_DLC_FILE' 'data/shop_vehicle.meta'
+-- data_file 'CONTENT_UNLOCKING_META_FILE' 'data/vehiclecontentunlocks.meta'
